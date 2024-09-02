58fdaf9f1c597aa3002ad8abe3b81e5b14a3be1a
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-02 21:05:44 +0530

    Add files via upload
    How to give Input

---
+++ Input.java
@@ -0,0 +1,13 @@
+package com.dp;
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		System.out.println("String is : " + str);
+	}
+}
