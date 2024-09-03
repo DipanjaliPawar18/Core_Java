fd164563b7be8922a92d31c9d7aa092b49e1ffbe
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-03 21:58:29 +0530

    Add files via upload
    How to print numbers from 1 to 100 without using a number

---
+++ WithoutNum.java
@@ -0,0 +1,15 @@
+package com.dp;
+
+public class WithoutNum {
+	
+	public static void main(String args[]) {
+		
+		int a = 'a'/'a';
+		String str = "          ";
+		
+		for(int i = a; i <= str.length() * str.length(); i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
