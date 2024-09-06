6124db6ce74c598cdd5e754cf34174ce3d1a65db
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-06 22:27:25 +0530

    Add files via upload
    Method overriding in java

---
+++ Override.java
@@ -0,0 +1,27 @@
+package com.dp;
+
+class ParentMethodOverriding {
+
+	String name;
+	
+	//method
+	public void print_info(String name) {
+		System.out.println("name of parent = "+name);
+	}
+} 
+
+class ChildMethodOverriding {
+
+	public void print_info(String name) {
+		System.out.println("Child name : "+name);
+	}
+}
+
+public class Override {
+
+	public static void main(String[] args) {
+		ChildMethodOverriding c1=new ChildMethodOverriding();
+		//c1.name="child";
+		c1.print_info("child");
+	}
+}
