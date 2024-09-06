2af441e8388fbdac85b0eb3b091a5fddd45f290e
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-06 22:16:07 +0530

    Delete ActivaPlus.java

--- ActivaPlus.java
+++
@@ -1,9 +0,0 @@
-package com.inheritance;
-
-public class ActivaPlus extends Activa {
-
-	ActivaPlus(){
-		//super(); // will call the constructor from parents class
-		System.out.println("I have petrol engine and also self start feature");
-	}
-}
